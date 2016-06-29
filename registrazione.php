b9c4a0c1940d71ebdd946478c7983af1964f8cb0
LiVuoiQueiKiwi <gian.magic9618@gmail.com>
2016-06-29 10:13:19 +0200

    Update registrazione.php

--- registrazione.php
+++ registrazione.php
@@ -19,27 +19,26 @@
 			$mail=$_POST["email"];
 			$user=$_POST["username"];
 			$password=$_POST["password"];
-            if (!($nome=="" || $cognome=="" || $mail =="" || $user=="" || $password=="")){
-                $password=sha1($password);
-                $sql="	SELECT username, email
-                        FROM utenti;";
-                foreach($this->pdo->query($sql) as $row){
-                    if($user==$row['username'] || $mail==$row['email'])  {
-                        header("location: popupRL.html");	
-                        exit(0);
-                    }
-
-
-                }
+            		
+            		if (!($nome=="" || $cognome=="" || $mail =="" || $user=="" || $password=="")){
+                		$password=sha1($password);
+                		$sql="	SELECT username, email
+                        		FROM utenti;";
+	                	foreach($this->pdo->query($sql) as $row){
+	                    		if($user==$row['username'] || $mail==$row['email'])  {
+	                        	header("location: popupRL.html");	
+	                        	exit(0);
+	                    		}
+	                	}
                 
-                $dml=" INSERT INTO utenti (nome,cognome,username,email,password) VALUES('$nome','$cognome','$user','$mail','$password')";
+	                	$dml=" INSERT INTO utenti (nome,cognome,username,email,password) VALUES('$nome','$cognome','$user','$mail','$password')";
 				$res=$this->pdo->exec($dml);		
 				$_SESSION['username'] = $user;
 				header("location: Main.php");
 			}
-            else header("location: popupRL.html");
-        }
-    }
+            		else header("location: popupRL.html");
+        	}
+    	}
 
 	session_start();
 	$R=new Registrazione();
