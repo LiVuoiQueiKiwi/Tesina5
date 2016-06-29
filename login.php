2a0114bc154db53e9df70ab6036b6e256ae28d68
LiVuoiQueiKiwi <gian.magic9618@gmail.com>
2016-06-29 10:08:48 +0200

    Update login.php

--- login.php
+++ login.php
@@ -16,26 +16,26 @@
 			
 			$user=$_POST["Username"];
 			$password=$_POST["Password"];
-            $password=sha1($password);
-            if (!($user=="" || $password=="")){
-                $sql="	SELECT username, password
-					    FROM utenti;";
-                foreach($this->pdo->query($sql) as $row){
-				    if($user==$row['username']){
-					   if($password==$row['password']){
-						//accesso corretto. vai alla pagina home
-						$_SESSION['username'] = $user;
-						header("location: main.php");
-                        exit(0);
-					   } else
-                            //ritorna alla pagina login con messaggio "Password sbagliata!"
-                            header("location: loginpage.php"); 
-                    }
-                    else header("location: loginpage.php"); //errore username
-			     }
-            }
-            else header("location: popupRL.html");
-			//ritorna alla pagina login con messaggio "Nome utente sbagliato o non registrato!"
+            		$password=sha1($password);
+            	if (!($user=="" || $password=="")){
+                	$sql="	SELECT username, password
+				FROM utenti;";
+                	foreach($this->pdo->query($sql) as $row){
+				if($user==$row['username']){
+				   if($password==$row['password']){
+					//accesso corretto. vai alla pagina home
+					$_SESSION['username'] = $user;
+					header("location: main.php");
+                        		exit(0);
+				   } else
+                            	//ritorna alla pagina login con messaggio "Password sbagliata!"
+                            	header("location: loginpage.php"); 
+                    		}
+                    		else header("location: loginpage.php"); //errore username
+		    	}
+            	}
+            	else header("location: popupRL.html");
+		//ritorna alla pagina login con messaggio "Nome utente sbagliato o non registrato!"
 			
 		}
 	}
